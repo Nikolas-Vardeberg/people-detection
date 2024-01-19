e569f9355779fa634f772649d2be5efd6b0c3187
niklusrook <pantebolle@gmail.com>
2024-01-19 08:06:25 +0100

    css, detection

---
+++ utils/draw.ts
@@ -0,0 +1,35 @@
+import { DetectedObject } from "@tensorflow-models/coco-ssd";
+
+export function drawOnCanvas(
+    mirrored: boolean,
+    predictions: DetectedObject[],
+    ctx: CanvasRenderingContext2D | null | undefined
+) {
+    predictions.forEach((detectedObject: DetectedObject) => {
+        const { class: name, bbox, score} = detectedObject;
+        const [x, y, width, height] = bbox;
+
+        if(ctx) {
+            ctx.beginPath();
+
+            
+            ctx.fillStyle = name === "person" ? "#ff0f0f" : "00b612";
+            ctx.globalAlpha = 0.4;
+
+            mirrored ? 
+            ctx.roundRect(ctx.canvas.width - x, y , -width, height, 8)
+            : ctx.roundRect(x, y, width, height, 8)
+
+            ctx.fill()
+
+            ctx.font = "12px Courier new"
+            ctx.globalAlpha = 1;
+            ctx.fillStyle = "black"
+            
+            mirrored
+            ? ctx.fillText(name, ctx.canvas.width - x - width + 10, y + 20)
+            : ctx.fillText(name, x+ 10, y + 20)
+
+        }
+    })
+}
